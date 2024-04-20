1e1194c84e0af7c84680fd671f7c22ded5ad1637
Ganesh <ganesh@webenertia.com>
2024-04-20 12:37:19 +0530

    update design feedback

--- components/StickyNav.jsx
+++ components/StickyNav.jsx
@@ -179,7 +179,7 @@ const StickyNav = (props) => {
                             {data.blurb}
                           </p>
                           <div className={`${style.arrow}`}>
-                            <Image className='w-full h-full max-h-[30px] max-w-[20px]' src="/images/icons/arrow.svg" width={100} height={100} alt="Arrow" />
+                            <Image className='w-full h-full max-h-[33px] max-w-[25px]' src="/images/icons/arrow.svg" width={100} height={100} alt="Arrow" />
                           </div>
                         </div>
                       ) : null
