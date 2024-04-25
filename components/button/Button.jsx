9fa50572344e2658e4b5d188b611ee6a34df4bc0
Atiksha <atiksha@webenertia.com>
2024-04-25 15:02:22 +0530

    Update

--- components/button/Button.jsx
+++ components/button/Button.jsx
@@ -1,16 +1,35 @@
+import React from 'react';
 import Link from "next/link";
 import VideoButton from "./VideoBtn";
 
-const Button = ({ buttonText, url, target, buttonClass, videoUrl }) => {
+const isExternal = (url) => {
+  try {
+    const currentHost = window.location.hostname;
+    const urlHost = new URL(url).hostname;
+    return currentHost !== urlHost;
+  } catch (error) {
+    console.error(`Invalid URL: ${url}`);
+    return false;
+  }
+};
 
+
+const Button = ({ buttonText, url, target, buttonClass, videoUrl }) => {
+  let linkTarget;
+  if (url !== "/" && url !== "#" && url !== "") {
+    linkTarget = target || (isExternal(url) ? "_blank" : "");
+  }
   return (
     <>
-      {videoUrl ? (<VideoButton buttonText={buttonText} url={url} buttonClass={buttonClass} videoUrl={videoUrl}/>) : 
-      ( <Link href={url} target={target ? "_blank":""} className={`button ${buttonClass}`}>{buttonText}</Link>)
+      {videoUrl ? (<VideoButton buttonText={buttonText} url={url} buttonClass={buttonClass} videoUrl={videoUrl} />) :
+        (<Link href={url} target={linkTarget} className={`button ${buttonClass}`}>{buttonText}</Link>)
       }
     </>
-   
+
   )
 }
 
 export default Button;
+
+
+
