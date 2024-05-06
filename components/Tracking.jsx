fdd8fd4c3bb7e87afd8a2b18f20db50f795938dd
Ganesh <ganesh@webenertia.com>
2024-05-06 14:59:50 +0530

    sync code

--- components/Tracking.jsx
+++ components/Tracking.jsx
@@ -2,15 +2,9 @@ import Script from 'next/script'
 
 export const HeaderTracking = () => {
     return (
-        <>
-        <Script 
+        <Script
             strategy="beforeInteractive"
             id='gtm-script'
-            dangerouslySetInnerHTML={{__html: `console.log('test')`,}}/>
-        </>
+            dangerouslySetInnerHTML={{ __html: `console.log('test')`, }} />
     )
 }
-
-// export const bodyGTM = () => {
-//     return 
-// }
