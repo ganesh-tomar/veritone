6c5a7aab22213fccc4e21e8bfc50d6175eddfe29
Ganesh <ganesh@webenertia.com>
2024-05-06 15:09:03 +0530

    sync code

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,26 +1,25 @@
 import '../styles/globals.css';
 import '../components/button/button.css';
 import '../styles/checkboxSwitch.css';
-import ChatBot from '../components/ChatBot';
 import { HeaderTracking } from '../components/Tracking';
 import Head from 'next/head';
 
-// import '@pantheon-systems/nextjs-kit/style.css';
 import Layout from '../components/layout';
 
 function MyApp({ Component, pageProps }) {
 	return (
 		<>
 			<Head>
-
-
-
-
 				<HeaderTracking />
 			</Head>
 			<Layout>
-				<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K8DLN47"
-					height="0" width="0" style={{ display: 'none', visibility: 'hidden' }}></iframe></noscript>
+				<noscript><iframe
+					src="https://www.googletagmanager.com/ns.html?id=GTM-K8DLN47"
+					height="0"
+					width="0"
+					style={{ display: 'none', visibility: 'hidden' }}
+					title="Google Tag Manager"
+				></iframe></noscript>
 				<Component {...pageProps} />
 			</Layout>
 
