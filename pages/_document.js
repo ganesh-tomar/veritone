b5ee78a074dc6406e24f399b98c07cdbbce174d5
Ganesh <ganesh@webenertia.com>
2024-05-07 14:36:17 +0530

    update metadata

--- pages/_document.js
+++ pages/_document.js
@@ -1,35 +1,40 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
-	render() {
-		return (
-			<Html lang="en">
-				<Head>
-                <meta name="twitter:card" content="summary_large_image" />
-				<meta name="twitter:title" content="Veritone | AI that makes you even better" />
-				<meta name="twitter:description" content="Transform your workflows and superpower your teams with innovative, custom AI solutions, AI services, and a powerful enterprise AI platform." />
-				{/* <meta name="twitter:image" content="https://nextjs.veritone.com/images/veritone_OG_image.jpg" /> */}
-				<meta name="twitter:site" content="https://twitter.com/veritoneinc" />
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <meta name="twitter:card" content="summary_large_image" />
+          {/* <meta name="twitter:title" content="Veritone | AI that makes you even better" />
+				<meta name="twitter:description" content="Transform your workflows and superpower your teams with innovative, custom AI solutions, AI services, and a powerful enterprise AI platform." /> */}
+          {/* <meta name="twitter:image" content="https://nextjs.veritone.com/images/veritone_OG_image.jpg" /> */}
+          <meta name="twitter:site" content="https://twitter.com/veritoneinc" />
 
-                <script strategy="lazyOnload"
-					dangerouslySetInnerHTML={{
-						__html: `  window.dataLayer = window.dataLayer || [];
+          <script
+            strategy="lazyOnload"
+            dangerouslySetInnerHTML={{
+              __html: `  window.dataLayer = window.dataLayer || [];
 					function gtag(){dataLayer.push(arguments);}
 					gtag('js', new Date());
 				  
-					gtag('config', 'G-V9F3W9WQ5R');`}}
-				/>
-                <script strategy="lazyOnload"
-					dangerouslySetInnerHTML={{
-						__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+					gtag('config', 'G-V9F3W9WQ5R');`,
+            }}
+          />
+          <script
+            strategy="lazyOnload"
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 					new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 					j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 					'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-					})(window,document,'script','dataLayer','GTM-K8DLN47');`}}
-				/>
-				<script strategy="lazyOnload"
-					dangerouslySetInnerHTML={{
-						__html: `window.addEventListener('message', function()
+					})(window,document,'script','dataLayer','GTM-K8DLN47');`,
+            }}
+          />
+          <script
+            strategy="lazyOnload"
+            dangerouslySetInnerHTML={{
+              __html: `window.addEventListener('message', function()
 					{
 					  if(event.data.sendToGA)
 					{
@@ -38,10 +43,13 @@ class MyDocument extends Document {
 					   gtag('event', eventName, {});
 					}
 					}
-					);`}}
-				/>
-                <script strategy="lazyOnload"
-					dangerouslySetInnerHTML={{__html:`	function initPardot() {
+					);`,
+            }}
+          />
+          <script
+            strategy="lazyOnload"
+            dangerouslySetInnerHTML={{
+              __html: `	function initPardot() {
 						if (window.pardotDidInit) {
 							return false;
 						}
@@ -53,26 +61,25 @@ class MyDocument extends Document {
 						var c = document.getElementsByTagName('script')[0];
 						c.parentNode.insertBefore(script, c);
 					}
-					initPardot()`}}
-				/>
-				<script strategy="lazyOnload"
-					dangerouslySetInnerHTML={{__html:`piAId = '637301';
+					initPardot()`,
+            }}
+          />
+          <script
+            strategy="lazyOnload"
+            dangerouslySetInnerHTML={{
+              __html: `piAId = '637301';
 					piCId = '20529';
-					piHostname = 'pi.pardot.com';`}}
-				/>
-				</Head>
-				<body>
-					<Main />
-					<NextScript />
-				</body>
-			</Html>
-		);
-	}
+					piHostname = 'pi.pardot.com';`,
+            }}
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
 
 export default MyDocument;
-
-
-
-
-
