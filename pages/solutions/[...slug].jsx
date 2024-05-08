2832d0a12377c4669c159a868bf34cb3c7fd9f5a
Ganesh <ganesh@webenertia.com>
2024-05-08 18:51:59 +0530

    update og image

--- pages/solutions/[...slug].jsx
+++ pages/solutions/[...slug].jsx
@@ -39,105 +39,48 @@ const Home = () => {
 	};
 
 	const router = useRouter();
-	if (router.asPath === '/solutions/media-entertainment') {
-		return (
-			<>
-				<NextSeo
-					title="Veritone | AI that makes you even better"
-					description="Transform your workflows and superpower your teams with innovative, custom AI solutions, AI services, and a powerful enterprise AI platform."
-					canonical='https://www.veritone.com'
-					openGraph={{
-						url: 'https://www.veritone.com',
-						title: 'Veritone | AI that makes you even better',
-						description: 'Transform your workflows and superpower your teams with innovative, custom AI solutions, AI services, and a powerful enterprise AI platform.',
-						images: [
-							{
-								url: 'https://nextjs.veritone.com/images/veritone_OG_image.jpg',
-								width: 800,
-								height: 600,
-								alt: 'Veritone',
-								type: 'image/jpg',
-							},
-						],
-						siteName: 'Veritone',
-					}}
-					twitter={{
-						handle: '@handle',
-						site: '@site',
-						cardType: 'summary_large_image',
-					}}
-				/>
-				<BannerSecondLevel {...banner} setFormOverlay={setFormOverlay} />
-				<StickyNav data={stickyNavData} themeColor={themeColor} />
-				<IntroWithAccordion data={introWithAccordionData1} pt="padding-top-120" pb='no-padding-bottom' />
-				<IntroWithAccordion data={introWithAccordionData2} reverse={"true"} pt='padding-medium-top' pb='no-padding-bottom' />
-				<IntroWithAccordion data={introWithAccordionData3} pt='padding-medium-top' pb='no-padding-bottom' />
-				<IntroWithAccordion data={introWithAccordionData4} reverse={"true"} pt='padding-medium-top' pb='padding-medium-bottom' />
-				<Tabs data={tabs} />
-				<Accordion data={accordionData} />
-				<ColTwoCards data={colTwoCards} />
-				<ResourceCards data={resourceCards} />
-				<TwitterSlider data={twitterSliderData} onlyInternalPosts={true} />
-				<Footercta {...footerCta} setFormOverlay={setFormOverlay} />
-				<FormOverlay toggle={open} setFormOverlay={setFormOverlay} buttonText={'Connect with us'} url={'#'} buttonClass={'default mr-[30px]'} formUrl={url} />
-			</>
-		);
-	} else if (router.asPath === '/solutions/public-sector') {
-		return (
-			<>
-				<NextSeo title="AI Solutions for Media, Entertainment & Broadcast | Veritone" description="Transform your media & entertainment business with Veritone's AI solutions. Drive growth and innovation. Click now." />
-				<BannerSecondLevel {...publicBanner} setFormOverlay={setFormOverlay} />
-				<StickyNav data={publicStickyNavData} themeColor={publicthemeColor} />
-				<IntroWithAccordion data={publicIntroWithAccordionData1} pt="padding-top-120" pb='no-padding-bottom' />
-				<IntroWithAccordion data={publicIntroWithAccordionData2} reverse={"true"} pt='padding-medium-top' pb='no-padding-bottom' />
-				<IntroWithAccordion data={publicIntroWithAccordionData3} pt='padding-medium-top' pb='no-padding-bottom' />
-				<IntroWithAccordion data={publicIntroWithAccordionData4} reverse={"true"} pt='padding-medium-top' pb='padding-medium-bottom' />
-				<Tabs data={publicTabs} />
-				<Accordion data={publicAccordionData} />
-				<ColTwoCards data={publicColTwoCards} />
-				<ResourceCards data={publicResourceCards} />
-				<TwitterSlider data={publicTwitterSliderData} onlyInternalPosts={true} />
-				<Footercta {...publicFooterCta} setFormOverlay={setFormOverlay} />
-				<FormOverlay toggle={open} setFormOverlay={setFormOverlay} buttonText={'Connect with us'} url={'#'} buttonClass={'default mr-[30px]'} formUrl={url} />
-			</>
-		);
-	} else if (router.asPath === '/solutions/hire') {
-		return (
-			<>
-				<NextSeo
-					title={metaData.title}
-					description={metaData.description}
-					openGraph={{
-						url: 'https://www.veritone.com',
-						images: [
-							{
-								url: metaData.ogImagePath,
-								width: 800,
-								height: 600,
-								alt: 'Veritone',
-								type: 'image/jpeg',
-							},
-						],
-						siteName: 'Veritone',
-					}}
-				/>
-
-				<BannerSecondLevel {...hireBanner} setFormOverlay={setFormOverlay} />
-				<StickyNav data={hireStickyNavData} themeColor={hireThemeColor} />
-				<IntroWithAccordion data={hireIntroWithAccordionData1} pt="padding-top-120" pb='no-padding-bottom' />
-				<IntroWithAccordion data={hireIntroWithAccordionData2} reverse={"true"} pt='padding-medium-top' pb='no-padding-bottom' />
-				<IntroWithAccordion data={hireIntroWithAccordionData3} pt='padding-medium-top' reverse={"true"} pb='no-padding-bottom' />
-				<IntroWithAccordion data={hireIntroWithAccordionData4} pt='padding-medium-top' pb='padding-medium-bottom' />
-				<Tabs data={hireTabs} />
-				{/* <Accordion data={hireAccordionData} /> */}
-				<ColTwoCards data={hireColTwoCards} />
-				<ResourceCards data={hireResourceCards} />
-				<TwitterSlider data={hireTwitterSliderData} onlyInternalPosts={true} />
-				<Footercta {...hireFooterCta} setFormOverlay={setFormOverlay} />
-				<FormOverlay toggle={open} setFormOverlay={setFormOverlay} buttonText={'Connect with us'} url={'#'} buttonClass={'default mr-[30px]'} formUrl={url} />
-			</>
-		);
-	}
+	return (
+		<>
+			<NextSeo
+				title="Veritone | AI that makes you even better"
+				description="Transform your workflows and superpower your teams with innovative, custom AI solutions, AI services, and a powerful enterprise AI platform."
+				canonical='https://www.veritone.com'
+				openGraph={{
+					url: 'https://www.veritone.com',
+					title: 'Veritone | AI that makes you even better',
+					description: 'Transform your workflows and superpower your teams with innovative, custom AI solutions, AI services, and a powerful enterprise AI platform.',
+					images: [
+						{
+							url: 'https://nextjs.veritone.com/images/veritone_OG_image.jpg',
+							width: 800,
+							height: 600,
+							alt: 'Veritone',
+							type: 'image/jpg',
+						},
+					],
+					siteName: 'Veritone',
+				}}
+				twitter={{
+					handle: '@handle',
+					site: '@site',
+					cardType: 'summary_large_image',
+				}}
+			/>
+			<BannerSecondLevel {...banner} setFormOverlay={setFormOverlay} />
+			<StickyNav data={stickyNavData} themeColor={themeColor} />
+			<IntroWithAccordion data={introWithAccordionData1} pt="padding-top-120" pb='no-padding-bottom' />
+			<IntroWithAccordion data={introWithAccordionData2} reverse={"true"} pt='padding-medium-top' pb='no-padding-bottom' />
+			<IntroWithAccordion data={introWithAccordionData3} pt='padding-medium-top' pb='no-padding-bottom' />
+			<IntroWithAccordion data={introWithAccordionData4} reverse={"true"} pt='padding-medium-top' pb='padding-medium-bottom' />
+			<Tabs data={tabs} />
+			<Accordion data={accordionData} />
+			<ColTwoCards data={colTwoCards} />
+			<ResourceCards data={resourceCards} />
+			<TwitterSlider data={twitterSliderData} onlyInternalPosts={true} />
+			<Footercta {...footerCta} setFormOverlay={setFormOverlay} />
+			<FormOverlay toggle={open} setFormOverlay={setFormOverlay} buttonText={'Connect with us'} url={'#'} buttonClass={'default mr-[30px]'} formUrl={url} />
+		</>
+	);
 };
 
 export default Home;
