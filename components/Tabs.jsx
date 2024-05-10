48dd78b461588dfd312ac14cbfe383d2a69629cc
Ganesh <ganesh@webenertia.com>
2024-05-10 14:04:03 +0530

    sync

--- components/Tabs.jsx
+++ components/Tabs.jsx
@@ -2,11 +2,8 @@ import React from 'react'
 import TabWithSlider from './tabVariations/TabWithSlider'
 import TabWithoutSlider from './tabVariations/TabWithoutSlider'
 
-
 const Tabs = (props) => {
-
   let bladeData = props.data
-
   return (
     <>
       {bladeData?.slider ? (<TabWithSlider {...bladeData} />) : (<TabWithoutSlider {...bladeData} />)}
