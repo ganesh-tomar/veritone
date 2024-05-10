b80a44bde7e0a3f99fbcc1647b0a2db4486fab02
Ganesh <ganesh@webenertia.com>
2024-05-10 13:56:28 +0530

    sync

--- components/Footercta.jsx
+++ components/Footercta.jsx
@@ -1,5 +1,4 @@
-import React, { useEffect } from "react";
-import Link from "next/link";
+import React from "react";
 import Button from "./button/Button";
 import ContactUs from "./button/ContactUs";
 
